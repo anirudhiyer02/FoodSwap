3470ba1d2b585d8c5aed24449fc288251faa25a9
Anirudh Iyer <anirudhiyer@anirudhs-macbook-pro.local>
2019-01-30 12:38:09 -0800

    attempt

---
+++ FoodSwap/FoodSwap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodSwap
+//
+//  Created by Anirudh Iyer on 1/29/19.
+//  Copyright © 2019 Anirudh Iyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
